6b6d022d328080797ff6dc8993cff6a9c5e7185d
Minos Gabioud <minosgabioud@gmail.com>
2023-02-24 08:25:29 +0100

    Add storage of CPU load and add a second fake device, with doubled CPU load and Memory usage

--- src/SystemPropertiesPolling/SystemProperties.java
+++ src/SystemPropertiesPolling/SystemProperties.java
@@ -5,6 +5,8 @@ import Measurement.Utility;
 import com.sun.management.OperatingSystemMXBean;
 import java.lang.management.ManagementFactory;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
+
 /**
  * The class allows reading the RAM usage parameters and pushes them to the DatabaseConnector.
  */
@@ -14,6 +16,8 @@ public class SystemProperties {
     public double cpuLoad;
     public double usedMemorySize;
     private int burst;
+    private static final String DEVICE_LP = "D_LP";
+    private static final String DEVICE_HP = "D_HP";
 
     private OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
 
@@ -34,23 +38,35 @@ public class SystemProperties {
      *  Push the used RAM size to the Database
      */
     public void pushToDbFull() {
-        Point point = new Point("MemoryUsage");
-        point.addTag("PC", "W29");
+        Point point = new Point("SystemProperties");
+        point.addTag("Device", DEVICE_LP);
         point.addFloatField("MemoryUsageFull", (float) (usedMemorySize/1E6));
+        point.addFloatField("CpuLoadFull", (float)(cpuLoad));
+        point.request();
 
+        point = new Point("SystemProperties");
+        point.addTag("Device", DEVICE_HP);
+        point.addFloatField("MemoryUsageFull", (float) (usedMemorySize/1E6 * 2));
+        point.addFloatField("CpuLoadFull", (float)(Math.max(cpuLoad * 2, 1)));
         point.request();
     }
 
     /**
-     * Pushe some used RAM size to the database
+     * Push some used RAM size to the database
      * @param rate fraction of values to push to the database
      */
     public void pushToDbRandom(double rate) {
         if (Math.random() < rate) {
             Point point = new Point("MemoryUsage");
-            point.addTag("PC", "W29");
+            point.addTag("Device", DEVICE_LP);
             point.addFloatField("MemoryUsageRandom", (float) (usedMemorySize/1E6));
+            point.addFloatField("CpuLoadRandom", (float)(cpuLoad));
+            point.request();
 
+            point = new Point("SystemProperties");
+            point.addTag("Device", DEVICE_HP);
+            point.addFloatField("MemoryUsageRandom", (float) (usedMemorySize/1E6 * 2));
+            point.addFloatField("CpuLoadRandom", (float)(Math.max(cpuLoad * 2, 1)));
             point.request();
         }
     }
@@ -65,9 +81,15 @@ public class SystemProperties {
             burst--;
 
             Point point = new Point("MemoryUsage");
-            point.addTag("PC", "W29");
+            point.addTag("Device", DEVICE_LP);
             point.addFloatField("MemoryUsageBurst", (float) (usedMemorySize/1E6));
+            point.addFloatField("CpuLoadBurst", (float)(cpuLoad));
+            point.request();
 
+            point = new Point("SystemProperties");
+            point.addTag("Device", DEVICE_HP);
+            point.addFloatField("MemoryUsageBurst", (float) (usedMemorySize/1E6 * 2));
+            point.addFloatField("CpuLoadBurst", (float)(Math.max(cpuLoad * 2, 1)));
             point.request();
         }
         else {
